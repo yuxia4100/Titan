fa309f6c26634d77507c534e30d36f1edffd3f36
yuxia4100 <yuxia4100@gmail.com>
2017-07-27 21:06:32 -0700

    Add MongoDb Support.

--- src/db/DBConnectionFactory.java
+++ src/db/DBConnectionFactory.java
@@ -1,9 +1,10 @@
 package db;
 
+import db.mongodb.MongoDBConnection;
 import db.mysql.MySQLConnection;
 
 public class DBConnectionFactory {
-	private static final String DEFAULT_DB = "mysql";
+	private static final String DEFAULT_DB = "mongodb";
 
 	// Create a DBConnection based on given db type.
 	public static DBConnection getDBConnection(String db) {
@@ -11,7 +12,10 @@ public class DBConnectionFactory {
 		// You may try other dbs and add them here.
 		case "mysql":
 			return MySQLConnection.getInstance();
-
+		
+		case "mongodb":
+			return MongoDBConnection.getInstance();
+			
 		default:
 			throw new IllegalArgumentException("Invalid db " + db);
 		}
